80ab7e718241f687422d8d99a7958ff1c9019452
GAD ELMOZNINO <Gad@GAD-MacBook-2.local>
2015-06-08 13:04:05 -0400

    Initial Commit

---
+++ CircuitSolver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircuitSolver
+//
+//  Created by GAD ELMOZNINO on 2015-06-08.
+//  Copyright (c) 2015 Eric Elmoznino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
